8bd1213786d93dd527bf7ddf14669b4c4e12528b
Sagar Kakade <ksagard@yahoo.com>
2012-02-13 16:18:19 -0800

    successful script

---
+++ scripts/fetch.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+mkdir ~/openwrt_build
+cd ~/openwrt_build
+svn co svn://svn.openwrt.org/openwrt
+cd ~/openwrt_build/openwrt/trunk
+
+#./scripts/feeds update -a
+#./scripts/feeds install -a

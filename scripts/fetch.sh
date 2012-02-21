677a3533e278f6625e73af3ca56c4360495ef656
Sagar Kakade <ksagard@yahoo.com>
2012-02-21 12:07:53 -0800

    scripts updated

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,14 +1,6 @@
 #! /bin/sh
 
 set -x
-BUILDROOT=openwrt_11s/op
-ROOT_UID=0	#Only users with root privilege can access the script
-
-if [ "$UID" -ne "$ROOT_UID" ]
-then
-  echo "Must be root to run this script."
-  exit $E_NOTROOT
-fi  
 svn co svn://svn.openwrt.org/openwrt
 
 cp openwrt_11s/config_files/.config openwrt/trunk/

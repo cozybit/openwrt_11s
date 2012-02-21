8b9578504a95f126d5cfbfd4a0330624436fabbe
Sagar Kakade <ksagard@yahoo.com>
2012-02-21 14:32:20 -0800

    scripts updated

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
+set -x
 cd openwrt/trunk
+make defconfig
+cp openwrt_11s/config_files/.config openwrt/trunk/
 make
--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -3,8 +3,6 @@
 set -x
 svn co svn://svn.openwrt.org/openwrt
 
-cp openwrt_11s/config_files/.config openwrt/trunk/
-
 mkdir openwrt/trunk/files
 mkdir openwrt/trunk/files/etc
 mkdir openwrt/trunk/files/etc/config

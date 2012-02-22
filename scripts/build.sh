00309a554588ecd701f07e0513383180e27ee7ae
Javier Cardona <javier@cozybit.com>
2012-02-21 17:34:05 -0800

    Fix syntax error in script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-[ `basename $PWD` == openwrt_11s ] || { echo This script must be executed from openwrt_11s root directory; exit -1 }
+[ `basename $PWD` == openwrt_11s ] || { echo This script must be executed from openwrt_11s root directory; exit -1; }
 cp config_files/dot.config openwrt/.config
 cd openwrt
 make

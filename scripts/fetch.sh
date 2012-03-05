7b281df7649556895120c7bc047cb804383646ab
Javier Cardona <javier@cozybit.com>
2012-03-05 11:11:26 -0800

    Fetch external kernel and build against it. WIP

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -3,3 +3,4 @@
 [ `basename $PWD` == openwrt_11s ] || { echo This script must be executed from openwrt_11s root directory; exit -1; }
 
 svn co svn://svn.openwrt.org/openwrt/trunk openwrt
+git clone git@github.com:cozybit/open80211s kernel

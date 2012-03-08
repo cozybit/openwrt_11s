f6b7163d2ad0688ee1731e7acdaf54da4e28162c
Javier Cardona <javier@cozybit.com>
2012-03-07 18:22:46 -0800

    Use git repo for openwrt

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 
-OPENWRT_VERSION=30831
-
 [ `basename $PWD` == openwrt_11s ] || { echo This script must be executed from openwrt_11s root directory; exit -1; }
 
 # TODO: use git for openwrt as well
-svn co -r${OPENWRT_VERSION} svn://svn.openwrt.org/openwrt/trunk openwrt
+git clone git://nbd.name/openwrt.git
 git clone git@github.com:cozybit/open80211s kernel

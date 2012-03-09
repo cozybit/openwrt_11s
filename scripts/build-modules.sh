d17b9aeb122183b7f0fc329bb015b5d2b0d9b61c
Javier Cardona <javier@cozybit.com>
2012-03-08 21:46:11 -0800

    Partial fix for #2
    Build modules outside of openwrt build system.
    Install manually.
    Still not working because of a kernel config mismatch.

---
+++ scripts/build-modules.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+pushd openwrt/build_dir/linux-x86_generic/linux-*
+ARCH=x86 make modules
+ARCH=x86 INSTALL_MOD_PATH=/home/javier/dev/openwrt_11s/openwrt/build_dir/target-i386_uClibc-0.9.33/root-x86 make modules_install
+popd
+cd openwrt
+make target/install

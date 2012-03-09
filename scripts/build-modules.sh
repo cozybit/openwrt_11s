d75b7e44ab42ca16acd91f22aabf2b00b283e4ea
Javier Cardona <javier@cozybit.com>
2012-03-09 13:37:42 -0800

    New kernel config trying to get the modules to build.
    Currently breaking due to missing CONFIG_AVERAGE in base kernel.
    
    MODPOST 6 modules
    ERROR: "ewma_init" [net/mac80211/mac80211.ko] undefined!
    ERROR: "ewma_add" [net/mac80211/mac80211.ko] undefined!
    make[1]: *** [__modpost] Error 1

--- scripts/build-modules.sh
+++ scripts/build-modules.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+cp config_files/kernel.dot.config openwrt/build_dir/linux-x86_generic/linux-*/.config
 pushd openwrt/build_dir/linux-x86_generic/linux-*
 ARCH=x86 make modules
 ARCH=x86 INSTALL_MOD_PATH=/home/javier/dev/openwrt_11s/openwrt/build_dir/target-i386_uClibc-0.9.33/root-x86 make modules_install

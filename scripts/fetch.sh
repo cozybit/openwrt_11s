90442ae07064daec6a5f072edca55bd8e2609419
Javier Cardona <javier@cozybit.com>
2012-03-09 12:47:25 -0800

    Fix an openwrt commit id.
    No need to have moving openwrt versions for development.
    It's enought headaches with the linux kernel alone.

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
+OPENWRT_COMMIT=d118a7af950c6dbc364cf5c7d02cc139e6d3413c
+
 [ `basename $PWD` == openwrt_11s ] || { echo This script must be executed from openwrt_11s root directory; exit -1; }
 
 # TODO: use git for openwrt as well
-git clone git://nbd.name/openwrt.git
+git clone git://nbd.name/openwrt.git --no-checkout
+cd openwrt; git checkout ${OPENWRT_COMMIT}; cd ..
 git clone git@github.com:cozybit/open80211s kernel

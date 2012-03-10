31832232f2feee7ef1506746a14441798a50a5ac
Javier Cardona <javier@cozybit.com>
2012-03-09 18:24:18 -0800

    Install modules outside of openwrt build system.
    After trying really hard to find a way to do this through openwrt build
    system I gave up.  This scripts will just mount the rootfs, compile and
    add the modules to it.  Incidentally, it's much faster that anything
    else in the universe.
    
    [Fixes #2]

--- scripts/build-modules.sh
+++ scripts/build-modules.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 
+MNTPOINT=`mktemp -d`
+IMGFILE=openwrt/build_dir/linux-x86_generic/root.ext4
+LOOPDEV=`sudo losetup -s -f $IMGFILE`
+
+# mount openwrt's generated rootfs
+sudo mount -t ext4 $LOOPDEV $MNTPOINT
+
+# compile the modules and install there
 cp config_files/kernel.dot.config openwrt/build_dir/linux-x86_generic/linux-*/.config
 pushd openwrt/build_dir/linux-x86_generic/linux-*
 ARCH=x86 make modules
-ARCH=x86 INSTALL_MOD_PATH=/home/javier/dev/openwrt_11s/openwrt/build_dir/target-i386_uClibc-0.9.33/root-x86 make modules_install
-popd
-cd openwrt
-make target/install $*
+sudo ARCH=x86 INSTALL_MOD_PATH=${MNTPOINT} make modules_install
+
+# cleanup
+sudo umount $MNTPOINT
+sudo losetup -d $LOOPDEV

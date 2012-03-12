3d241fb5c56bf6f9b844fa256f316b1ca75e1f71
Javier Cardona <javier@cozybit.com>
2012-03-12 16:52:19 -0700

    Use the newer -device option in qemu to create an eth device

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -4,5 +4,10 @@ USER=`whoami`
 IFNAME=`sudo tunctl -u $USER -b`
 #TODO use root_check here
 sudo /etc/qemu-ifup $IFNAME
-qemu-system-i386 -nographic -kernel openwrt/build_dir/linux-x86_generic/bzImage -append "root=/dev/sda combined_mode=ide console=ttyS0 init=/etc/preinit" -net nic,model=e1000 -hda openwrt/build_dir/linux-x86_generic/root.ext4 -net tap,ifname=$IFNAME,script=no
+qemu-system-i386 -nographic -kernel openwrt/build_dir/linux-x86_generic/bzImage \
+	-hda openwrt/build_dir/linux-x86_generic/root.ext4 \
+	-append "root=/dev/sda combined_mode=ide console=ttyS0 init=/etc/preinit" \
+	-device e1000,netdev=lan0 \
+	-netdev tap,id=lan0,ifname=$IFNAME,script=no \
+	-enable-kvm -smp 2
 tunctl -d $IFNAME

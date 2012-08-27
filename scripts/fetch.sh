aa1210507ace41abb22be15a147380233f540daf
Jason Abele <jason@cozybit.com>
2012-08-27 16:14:03 +0900

    Port fetch.sh from spinach
    Bump openwrt version to match spinach
    Add patches/ directory for openwrt patches
    Lock feed versions

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -4,8 +4,18 @@ OPENWRT_COMMIT=d118a7af950c6dbc364cf5c7d02cc139e6d3413c
 
 [ `basename $PWD` == openwrt_11s ] || { echo This script must be executed from openwrt_11s root directory; exit -1; }
 
-git clone git://nbd.name/openwrt.git --no-checkout
-cd openwrt
-git checkout ${OPENWRT_COMMIT}
-./scripts/feeds update packages
-cd ..
+[ -d openwrt ] || { git clone git://nbd.name/openwrt.git --no-checkout || fail "ERROR: git clone failed. Aborting. "; }
+pushd openwrt
+git fetch
+git checkout ${OPENWRT_COMMIT} || fail "ERROR: git checkout failed. Aborting."
+
+# apply patches
+git am ../patches/* || fail "ERROR: patches werent able to apply cleanly. Aborting."
+
+# Clean out the feeds so we get the right rev on feeds update
+./scripts/feeds clean
+
+./scripts/feeds update
+./scripts/feeds install -a
+
+popd > /dev/null
